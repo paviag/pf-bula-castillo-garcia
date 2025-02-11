acbe01ffea723e5edf0f85b8e09ebdfaf5993bbd
unknown <aedavila@uninorte.edu.co>
2025-02-11 01:21:47 -0500

    Latest

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt") # No se si este es el correcto, en este link estan todos los modelos #https://docs.ultralytics.com/models/yolo11/#supported-tasks-and-modes
-model.train(data="pf-bula-castillo-garcia\data.yaml", epochs=5, imgsz=300) #no se si ese imgsz afecte el resultado por el resize automatico que hace
+model.train(data="pf-bula-castillo-garcia\data.yaml", epochs=5, imgsz=288) #no se si ese imgsz afecte el resultado por el resize automatico que hace
 
 model.export(format="onnx")
 
